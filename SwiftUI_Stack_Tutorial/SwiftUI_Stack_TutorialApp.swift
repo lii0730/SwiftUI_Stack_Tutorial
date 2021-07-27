8f45216f96b671294e73a06ba6c3a8aa161ab315
LeeHsss <lii0730@icloud.com>
2021-07-27 21:20:02 +0900

    Initial Commit

---
+++ SwiftUI_Stack_Tutorial/SwiftUI_Stack_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Stack_TutorialApp.swift
+//  SwiftUI_Stack_Tutorial
+//
+//  Created by LeeHsss on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Stack_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
